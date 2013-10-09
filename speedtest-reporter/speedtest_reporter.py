86a66c09cb640acfee360f91342904dc6a46c393
Olivier Watte - IPEOS <olivier@ipeos.com>
2013-10-08 21:25:58 -0400

    add copyright GNU GPL V3 digest in py source files

--- speedtest-reporter/speedtest_reporter.py
+++ speedtest-reporter/speedtest_reporter.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+    This file is part of speedtest-reporter.
+
+    Speedtest-reporter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Speedtest-reporter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with speedtest-reporter. If not, see <http://www.gnu.org/licenses/>
+'''
+
+
 import ConfigParser
 import datetime
 from speedtest_cli import *
